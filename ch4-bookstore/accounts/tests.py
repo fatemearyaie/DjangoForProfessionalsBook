5520d96f34de0043d459b6f3087c9361dd390962
fatemearyaie <fatemearyaie9@gmail.com>
2025-10-12 19:43:42 +0330

    add test for accounts

--- ch4-bookstore/accounts/tests.py
+++ ch4-bookstore/accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+class CostumUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='will', email='will@gmail.com', password='testpass123'
+        )
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username = 'superadmin', email='superadmin@gmail.com', password = '123456789'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_superuser)
+        self.assertTrue(admin_user.is_staff)

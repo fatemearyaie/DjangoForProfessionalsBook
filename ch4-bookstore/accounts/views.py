18c489e2138c0ae21990afa7617c8b2518c061a3
fatemearyaie <fatemearyaie9@gmail.com>
2025-10-14 08:38:54 +0330

    add signupview

--- ch4-bookstore/accounts/views.py
+++ ch4-bookstore/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import CostumUserCreationForm
+from django.views.generic import CreateView
+
 
 # Create your views here.
+class SignUpView(CreateView):
+    template_name = 'registration/signup.html'
+    form_class = CostumUserCreationForm
+    success_url = reverse_lazy('login')
+

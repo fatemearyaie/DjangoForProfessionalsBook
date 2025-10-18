e55d7e65f7d8dc7a5c9a9a4776bba03fd6157ea6
fatemearyaie <fatemearyaie9@gmail.com>
2025-10-18 15:04:27 +0330

    add allauth to the urls

--- ch4-bookstore/django_project/urls.py
+++ ch4-bookstore/django_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from pages.views import HomePageView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')),
     path('pages/', include('pages.urls')),
     path('', HomePageView.as_view(), name='home'),

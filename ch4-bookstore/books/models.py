36c6ee5de168a911ee75cb46c581597f48937fd4
fatemearyaie <fatemearyaie9@gmail.com>
2025-10-20 09:54:31 +0330

    add get_absolout_url

--- ch4-bookstore/books/models.py
+++ ch4-bookstore/books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=50)
@@ -8,3 +8,8 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", kwargs={"pk": self.pk})
+    
+    

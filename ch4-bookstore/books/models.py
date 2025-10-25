833fb5a29d207ffa90f09861619950c21419d06f
fatemearyaie <fatemearyaie9@gmail.com>
2025-10-25 09:29:50 +0330

    add review model

--- ch4-bookstore/books/models.py
+++ ch4-bookstore/books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=50)
@@ -13,3 +15,11 @@ class Book(models.Model):
         return reverse("book_detail", kwargs={"pk": self.pk})
     
     
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    review = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.review
